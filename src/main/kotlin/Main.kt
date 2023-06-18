9b7987ef5bd2427004863ab142ee81d6c36455c1
Rebeccanak <rebeccanakachwaaa@gmail.com>
2023-06-18 15:41:21 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,16 +11,24 @@ fun main() {
     println(buses.calculateParkingfees(10))
     buses.identity()
     buses.carry(4)
+    val wolf = Care("Toyota","KNB345","Pink",10)
+    wolf.carrying(12)
+    wolf.identity()
+    println(wolf.calculateParkingFes(5))
+    var big = Buses("Zuku","KB13","Indigo",13)
+    big.maxTripFare(5.0)
+    println(big.calculateParkingFes(6))
 
 
 }
-open class Person(var make :String, var model:String, var color:String, var capacity:Int){
+open class Car(var make :String, var model:String, var color:String, var capacity:Int){
     fun carry(people:Int){
-        var x= (people-capacity)
+//        var x= (people-capacity)
         if (people <= capacity){
             println("Carrying $people passengers")
         }
         else{
+            var x= (people-capacity)
             println("Over capacity $x people")
         }
     }
@@ -35,15 +43,12 @@ open class Person(var make :String, var model:String, var color:String, var capa
 }
 //Create a class Car with the following attributes: make, model, color, capacity.
 //It has these functions:
-open class Car( make :String,  model:String,  color:String,  capacity:Int):Person(make,model,color,capacity){
-//    he bus’ calculateParkingFees method returns the product of hours and
-//    the capacity of the bus
-}
 
 
 
 
-class Bus( make :String,  model:String,  color:String,  capacity:Int, var fare:Double):Person(make,model,color,capacity){
+
+class Bus( make :String,  model:String,  color:String,  capacity:Int, var fare:Double):Car(make,model,color,capacity){
 
     //    In addition, it has another method called maxTripFare(fare: Double) that
 //    calculates and returns the maximum amount of fare that can be collected per Trip
@@ -52,19 +57,11 @@ class Bus( make :String,  model:String,  color:String,  capacity:Int, var fare:D
         return (maximum)
     }
 
-
-
     override   fun calculateParkingfees(hours:Int):Int{
         var fees = hours*capacity
         return fees
-
     }
-
-
-
 }
-//Create a class Car with the following attributes: make, model, color, capacity.
-//It has these functions:
 //- carry(people: Int) : Prints out “Carrying $people passengers” if the
 //number of people is within its capacity else it prints out “Over capacity
 //by $x people” where x is the number of people exceeding its capacity
@@ -73,12 +70,40 @@ class Bus( make :String,  model:String,  color:String,  capacity:Int, var fare:D
 //format e.g: “I am a white subaru legacy” (1 point)
 //- calculateParkingFees(hours: Int) : Calculates and returns the
 //parking fees by multiplying the hours by 20 (1 point)
-//
+open class Care(var makes: String, var models: String, var colors: String, var capacitys: Int){
+    fun carrying(peoples: Int){
+        if (peoples <= capacitys){
+            println("Carryings $peoples passengers")
+        }
+        else{
+            var x = (peoples-capacitys)
+            println("Over capacity by $x people")
+        }
+    }
+    fun identity(){
+        println("I am a $colors $makes $models ")
+    }
+   open fun calculateParkingFes(hour: Int):Int{
+        var parkingFes = hour*20
+        return parkingFes
+    }
+}
 //2. Create a class Bus with the same attributes and functions as the Car class.
 //In addition, it has another method called maxTripFare(fare: Double) that
 //calculates and returns the maximum amount of fare that can be collected per
 //trip. (2 points)
 //The bus’ calculateParkingFees method returns the product of hours and
 //the capacity of the bus (3 points)
-//
 //Make use of inheritance to avoid duplication. Also be sure to instantiate both classes
+ class Buses( makes: String, models: String,  colors: String,  capacitys: Int):Care(makes,models,colors,capacitys){
+     fun maxTripFare(fare: Double):Double{
+         var maxmumfare = fare*capacitys
+         return maxmumfare
+     }
+
+    override fun calculateParkingFes(hour: Int): Int {
+        var fees = hour*capacitys
+        return fees
+    }
+ }
+
